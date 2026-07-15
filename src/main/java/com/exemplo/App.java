23f3906fbaa1ac9a6f51acb7c907b9fb5c558336
Erllison Reis <erllisonreis22@yahoo.com>
2026-07-15 14:40:55 -0400

    teste webhook

--- src/main/java/com/exemplo/App.java
+++ src/main/java/com/exemplo/App.java
@@ -3,7 +3,7 @@ package com.exemplo;
 public class App {
 
     public int soma(int a, int b){
-        System.out.println("Nova versã");
+        System.out.println("Nova versão");
 
         return a+b;
     }
